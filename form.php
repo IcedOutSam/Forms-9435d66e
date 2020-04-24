576848126e769d5367f409cdd50a8cf8f1801a76
Sam <2102926@talnet.nl>
2020-04-24 12:52:12 +0200

    Uitwerking van de exercise

--- form.php
+++ form.php
@@ -1,5 +1,14 @@
-<?php
-echo "<label for='email'> Enter your email: </label> ";
-echo "<input type='text' name='name' id='email'>";
-echo "<input type='button' value='Submit'> ";
- ?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <?php
+    echo "<label for='email'> Enter your email: </label> ";
+    echo "<input type='email' name='name' id='email'>";
+    echo "<input type='button' value='Submit'> ";
+     ?>
+  </body>
+</html>
